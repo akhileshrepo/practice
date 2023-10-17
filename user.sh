44dc84d7ef6341c34b10f16efdf1c49eb458588e
AKhilesh <kothapallyakhil123@gmail.com>
2023-10-17 14:15:05 +0530

    updated code

--- user.sh
+++ user.sh
@@ -13,7 +13,7 @@ cd /app
 npm install
 
 dnf install mongodb-org-shell -y
-mongo --host mongodb.akhildevops.online </app/schema/user.js
+mongo --host 172.31.22.169 </app/schema/user.js
 
 systemctl daemon-reload
 systemctl enable user
