62921718b089c2f1cce70b2e6592da382fd1e160
AKhilesh <kothapallyakhil123@gmail.com>
2023-10-19 10:50:01 +0530

    updated code

--- common.sh
+++ common.sh
@@ -1,20 +1,24 @@
+log=/tmp/roboshop.log
 func_apppreq() {
+  echo -e "\e[36m>>>>>>>>>>> create ${component} service <<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+  cp ${component}.service /etc/systemd/system/${component}.service
+
   echo -e "\e[36m>>>>>>>>>>> add app user <<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
-    useradd roboshop &>>${log}
+  useradd roboshop &>>${log}
 
-    echo -e "\e[36m>>>>>>>>>>> cleaning the app content <<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
-    rm -rf /app &>>${log}
+  echo -e "\e[36m>>>>>>>>>>> cleaning the app content <<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+  rm -rf /app &>>${log}
 
-    echo -e "\e[36m>>>>>>>>>>>create app directory<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
-    mkdir /app &>>${log}
+  echo -e "\e[36m>>>>>>>>>>>create app directory<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+  mkdir /app &>>${log}
 
-    echo -e "\e[36m>>>>>>>>>>> Download app content <<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
-    curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
+  echo -e "\e[36m>>>>>>>>>>> Download app content <<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
 
-    echo -e "\e[36m>>>>>>>>>>> Extract app content <<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
-    cd /app
-    unzip /tmp/${component}.zip  &>>${log}
-    cd /app
+  echo -e "\e[36m>>>>>>>>>>> Extract app content <<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+  cd /app
+  unzip /tmp/${component}.zip  &>>${log}
+  cd /app
 }
 
 func_systemD() {
@@ -26,9 +30,6 @@ func_systemD() {
 
 func_nodejs() {
   log=/tmp/roboshop.log
-
-  echo -e "\e[36m>>>>>>>>>>> create ${component} service <<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
-  cp ${component}.service /etc/systemd/system/${component}.service  &>>${log}
 
   echo -e "\e[36m>>>>>>>>>>> create Mongodb repo <<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
   cp mongo.repo /etc/yum.repos.d/mongo.repo &>>${log}
@@ -54,8 +55,7 @@ func_nodejs() {
 }
 
 func_java() {
-  echo -e "\e[36m>>>>>>>>>>> create ${component} service <<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
-  cp ${component}.service /etc/systemd/system/${component}.service
+
 
   echo -e "\e[36m>>>>>>>>>>> Install Maven <<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
   dnf install maven -y
@@ -73,4 +73,18 @@ func_java() {
   mysql -h 172.31.31.253 -uroot -pRoboShop@1 < /app/schema/${component}.sql
 
   func_systemD
+}
+
+func_python() {
+
+  echo -e "\e[36m>>>>>>>>>>> Build ${component}  service<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+  dnf install python36 gcc python3-devel -y
+
+  func_prerreq
+
+  echo -e "\e[36m>>>>>>>>>>> Install ${component} service <<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+  pip3.6 install -r requirements.txt
+
+  func_systemD
+
 }
