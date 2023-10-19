65904b08036951aac26390a34df3d5fdc9d298c1
AKhilesh <kothapallyakhil123@gmail.com>
2023-10-19 09:58:44 +0530

    updated code

--- common.sh
+++ common.sh
@@ -1,4 +1,30 @@
-nodejs() {
+func_apppreq() {
+  echo -e "\e[36m>>>>>>>>>>> add app user <<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+    useradd roboshop &>>${log}
+
+    echo -e "\e[36m>>>>>>>>>>> cleaning the app content <<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+    rm -rf /app &>>${log}
+
+    echo -e "\e[36m>>>>>>>>>>>create app directory<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+    mkdir /app &>>${log}
+
+    echo -e "\e[36m>>>>>>>>>>> Download app content <<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+    curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
+
+    echo -e "\e[36m>>>>>>>>>>> Extract app content <<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+    cd /app
+    unzip /tmp/${component}.zip  &>>${log}
+    cd /app
+}
+
+func_systemD() {
+  echo -e "\e[36m>>>>>>>>>>> start user service <<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+  systemctl daemon-reload  &>>${log}
+  systemctl enable ${component}  &>>${log}
+  systemctl restart ${component}  &>>${log}
+}
+
+func_nodejs() {
   log=/tmp/roboshop.log
 
   echo -e "\e[36m>>>>>>>>>>> create ${component} service <<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
@@ -13,22 +39,7 @@ nodejs() {
   echo -e "\e[36m>>>>>>>>>>> Install Node js <<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
   dnf install nodejs -y &>>${log}
 
-  echo -e "\e[36m>>>>>>>>>>> add app user <<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
-  useradd roboshop &>>${log}
-
-  echo -e "\e[36m>>>>>>>>>>> cleaning the app content <<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
-  rm -rf /app &>>${log}
-
-  echo -e "\e[36m>>>>>>>>>>>create app directory<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
-  mkdir /app &>>${log}
-
-  echo -e "\e[36m>>>>>>>>>>> Download app content <<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
-  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
-
-  echo -e "\e[36m>>>>>>>>>>> Extract app content <<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
-  cd /app
-  unzip /tmp/${component}.zip  &>>${log}
-  cd /app
+  func_apppreq
 
   echo -e "\e[36m>>>>>>>>>>> Download Nodejs dependencies <<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
   npm install &>>${log}
@@ -39,9 +50,27 @@ nodejs() {
   echo -e "\e[36m>>>>>>>>>>> Load schema <<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
   mongo --host 172.31.22.169 </app/schema/${component}.js  &>>${log}
 
-  echo -e "\e[36m>>>>>>>>>>> start user service <<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
-  systemctl daemon-reload  &>>${log}
-  systemctl enable ${component}  &>>${log}
-  systemctl restart ${component}  &>>${log}
+  func_systemD
+}
 
+func_java() {
+  echo -e "\e[36m>>>>>>>>>>> create ${component} service <<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+  cp ${component}.service /etc/systemd/system/${component}.service
+
+  echo -e "\e[36m>>>>>>>>>>> Install Maven <<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+  dnf install maven -y
+
+  func_apppreq
+
+  echo -e "\e[36m>>>>>>>>>>> Download Maven dependencies <<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+  mvn clean package
+  mv target/${component}-1.0.jar ${component}.jar
+
+  echo -e "\e[36m>>>>>>>>>>> Install Mysql client <<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+  dnf install mysql -y
+
+  echo -e "\e[36m>>>>>>>>>>> Load schema <<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+  mysql -h 172.31.31.253 -uroot -pRoboShop@1 < /app/schema/${component}.sql
+
+  func_systemD
 }
--- shipping.sh
+++ shipping.sh
@@ -1,20 +1,4 @@
-cp shipping.service /etc/systemd/system/shipping.service
+component=shipping
+source common.sh
 
-dnf install maven -y
-useradd roboshop
-mkdir /app
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
-cd /app
-unzip /tmp/shipping.zip
-cd /app
-mvn clean package
-mv target/shipping-1.0.jar shipping.jar
-
-
-dnf install mysql -y
-mysql -h 172.31.31.253 -uroot -pRoboShop@1 < /app/schema/shipping.sql
-
-
-systemctl daemon-reload
-systemctl enable shipping
-systemctl restart
+func_java
