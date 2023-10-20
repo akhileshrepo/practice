8edf01a8fd080a03e347c86a0d1ab4bab3995f6e
AKhilesh <kothapallyakhil123@gmail.com>
2023-10-20 15:25:02 +0530

    updated code

--- functions.sh
+++ functions.sh
@@ -1,7 +1,22 @@
 greeting() {
   echo Hello Good Morning,
   echo Hello Devops Trainers
+  return
   echo Im a Good learner
 }
 
 greeting
+echo Function exit status - $?
+
+# You declare the var in main programme, we can access it from functions and vice-versa
+# functions has its own special variables
+
+input() {
+  echo First Argument - $1
+  echo First Argument - $2
+  echo All arguments - $*
+  echo No of arguments - $#
+}
+input abc 1234
+
+
