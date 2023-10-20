d53d00c8e2e9071c9189028bf5471cfdd524b127
AKhilesh <kothapallyakhil123@gmail.com>
2023-10-20 11:33:35 +0530

    updated code

--- vars.sh
+++ vars.sh
@@ -1,3 +1,22 @@
 a=10
 
 echo A is $a
+
+#special variables
+# $0, $#, $N, $*
+
+#substitution variables
+## command substitution
+
+DATE=$(date)
+
+echo Today Date is $DATE
+
+##arithemetic substitution
+ADD=$(( 2+2 ))
+
+echo add 2+2 is $ADD
+
+
+
+
